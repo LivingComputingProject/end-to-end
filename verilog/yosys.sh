7de491cce0e9d758976611c82a1b8e7cab82207d
Timothy Jones <jonests@bu.edu>
2020-02-08 18:38:29 -0500

    opt_clean.

--- verilog/yosys.sh
+++ verilog/yosys.sh
@@ -1,3 +1,3 @@
 filename=$(basename -- "$1")
 filename="${filename%.*}"
-echo "read_verilog $1; flatten; splitnets -ports; hierarchy -auto-top; proc; techmap; opt; abc -g NOR; opt; hierarchy -auto-top; show -format png -prefix $filename-synthesis" | yosys
+echo "read_verilog $1; flatten; splitnets -ports; hierarchy -auto-top; proc; techmap; opt; abc -g NOR; opt; hierarchy -auto-top; opt_clean -purge; show -format png -prefix $filename-synthesis" | yosys
